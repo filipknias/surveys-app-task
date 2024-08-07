063df439e06414efd709e5f499c52a05201ee9a2
Filip Kniaś <filipknias@MacBook-Air-Filip.local>
2024-08-07 21:26:02 +0200

    default layout

---
+++ components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="bg-gray-800 py-6 px-2">
+    <div class="container mx-auto">
+        <div class="flex items-center flex-wrap">
+            <h1 class="text-white text-2xl mr-12">Surveys</h1>
+            <ul class="flex items-center gap-6 flex-wrap">
+                <NuxtLink to="/" class="text-white hover:underline">Home</NuxtLink>
+                <NuxtLink to="/surveys/new" class="text-white hover:underline">Create survey</NuxtLink>
+            </ul>
+        </div>
+    </div>
+  </nav>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="container mx-auto px-4">
+        <slot />
+    </div>
+  </div>
+</template>
