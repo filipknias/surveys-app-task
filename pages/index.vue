6b4b806eb1fefe455563643ed80c130291b09be7
Filip Kniaś <filipknias@MacBook-Air-Filip.local>
2024-08-13 09:42:36 +0200

    handle empty state

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,9 @@
             <label for="name" class="mb-2 block font-semibold text-lg">Find by name</label>
             <input type="text" id="name" v-model="name" class="border rounded-lg px-4 py-2 w-full" placeholder="Name..." />
         </div>
+        <div v-if="data.surveys.length === 0" class="bg-blue-200 rounded-lg px-8 py-4 mb-8">
+            <h1 class="text-blue-500 text-xl">No surveys found</h1>
+        </div>
         <div class="flex flex-wrap -m-4 mb-12">
             <div class="w-full lg:w-1/3 p-4" v-for="survey in data.surveys" :key="survey.id">
                 <NuxtLink :to="`/surveys/${survey._id}`">
@@ -35,11 +38,11 @@
 
     const route = useRoute()
     const router = useRouter()
-    const PER_PAGE = 1;
+    const PER_PAGE = 3;
     const name = ref("");
     const query = computed(() => ({ page: route.query.page, per_page: PER_PAGE, name: route.query.name }));
 
-    const { data, status, error, execute } = await useFetch<SurveysGetRequest>('/api/surveys', { query });
+    const { data, error } = await useFetch<SurveysGetRequest>('/api/surveys', { query });
     
     async function handlePageChange(page: number) {
         navigateTo({ 
--- server/api/surveys/index.get.ts
+++ server/api/surveys/index.get.ts
@@ -2,25 +2,27 @@ export default defineEventHandler(async (event) => {
     try {
         const { page, per_page, name } = getQuery(event);
         const currentPage = page || 1;
-        const pageLimit = per_page || 1;
-        const offset = (currentPage as number - 1) * (pageLimit as number);
+        const pageLimit = (per_page || 1) as number;
+        const offset = (currentPage as number - 1) * pageLimit;
 
         const totalCount = await Survey.countDocuments().exec();
-        const totalPages = Math.ceil(totalCount / (pageLimit as number));
+        const totalPages = Math.ceil(totalCount / pageLimit);
 
         if (name) {
-            const surveysByName = await Survey.find({ name: new RegExp(name as string, 'i') }).skip(offset).limit(pageLimit as number).exec();
+            const nameRegExp = new RegExp(name as string, 'i');
+            const queryTotalCount = await Survey.countDocuments({ name: nameRegExp }).exec()
+            const surveysByName = await Survey.find({ name: nameRegExp }).skip(offset).limit(pageLimit).exec();
             return {
                 surveys: surveysByName,
                 pagination: {
                     currentPage,
                     pageLimit,
-                    totalPages,
+                    totalPages: queryTotalCount,
                 },
             };
         }
 
-        const surveys = await Survey.find().skip(offset).limit(pageLimit as number).exec();
+        const surveys = await Survey.find().skip(offset).limit(pageLimit).exec();
         return {
             surveys,
             pagination: {
